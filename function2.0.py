6f7857c5bb1a0c7eef7b1b54bdef6c82df405cb6
harryhht <harryhht0@gmail.com>
2018-07-12 22:41:16 +0800

    Update Rubbish

--- function2.0.py
+++ function2.0.py
@@ -8,71 +8,101 @@ import pyttsx
 import re
 import urllib2
 import random
-
-data_raw = []
-data_sel = []
-
-
-def mixurl(input):
-    return 'http://fanyi.youdao.com/translate?&i=' + input + '&doctype=xml&version'
+import os
+from cmd import Cmd
 
 
-def trans(input):
-    request = urllib2.urlopen(mixurl(input))
-    xml = request.read()
-    p = r'[[](.*?)[]]'
-    pattern = re.compile(p)
-    ans = re.search(pattern, xml)
-    output = re.findall(pattern, xml)[1]
-    return output[6:]
+class Client(Cmd):
+    prompt = 'STUDY>'
+    intro = 'Demo Beta 0.1'
+    data_raw = []
+    data_sel = []
 
+    def __init__(self):
+        Cmd.__init__(self)
 
-def speak(input):
-    speak_engine = pyttsx.init()
-    rate = speak_engine.getProperty('rate')
-    speak_engine.setProperty('rate', rate - 50)
-    speak_engine.say(input)
-    speak_engine.runAndWait()
-    speak_engine.stop()
+    def do_import(self, input):
+        self.load_raw(input)
+        print len(self.data_raw)
 
+    def do_select(self, input):
+        self.select(input)
 
-def load_raw(input):
-    with open(input, 'r') as _DATA:
-        for line in _DATA:
-            if line != "":
-                data_raw.append(" ".join(line.split()))
-    _DATA.close()
+    def do_list(self, fuck):
+        self.printtrans(self.data_raw)
 
+    def do_train(self, input):
+        self.train(input)
 
-def select(heads):
-    for ele in data_raw:
-        if ele[0] in heads+fnn(heads):
-            data_sel.append(ele)
+    def do_exit(self, arg):
+        print 'Bye!'
+        return True  # 返回True，直接输入exit命令将会退出
 
+    def default(self, line):
+        print 'There\'s something wrong'
 
-def printtrans(list):
-    for ele in list:
-        print ele + ' ' + trans(ele)
+    def emptyline(self):
+        print 'please input command!'
 
-def fnn(input):
-    def fn(x):
-        if x.islower():
-            return x.upper()
-        elif x.isupper():
-            return x.lower()
-        else:
-            return x
-    return ''.join([fn(r) for r in list(input)])
+    def mixurl(self, input):
+        return 'http://fanyi.youdao.com/translate?&i=' + input + '&doctype=xml&version'
 
-def train(times):
-    for i in range(1,times):
-        out=data_sel[random.randint(0,len(data_sel)-1)]
-        raw_input(trans(out))
-        raw_input(out)
+    def trans(self, input):
+        request = urllib2.urlopen(self.mixurl(input))
+        xml = request.read()
+        p = r'[[](.*?)[]]'
+        pattern = re.compile(p)
+        # ans = re.search(pattern, xml)
+        output = re.findall(pattern, xml)[1]
+        return output[6:]
 
+    def speak(self, input):
+        speak_engine = pyttsx.init()
+        rate = speak_engine.getProperty('rate')
+        speak_engine.setProperty('rate', rate - 50)
+        speak_engine.say(input)
+        speak_engine.runAndWait()
+        speak_engine.stop()
+
+    def load_raw(self, input):
+        with open(input, 'r') as _DATA:
+            for line in _DATA:
+                if line != "":
+                    self.data_raw.append(" ".join(line.split()))
+        self.data_raw.sort()
+        _DATA.close()
+
+    def select(self, heads):
+        for ele in self.data_raw:
+            if ele[0] in heads + self.fnn(heads):
+                self.data_sel.append(ele)
+
+    def printtrans(self, IN_list):
+        for ele in IN_list:
+            print ele + ' ' + self.trans(ele)
+
+    def fnn(self, input):
+        def fn(x):
+            if x.islower():
+                return x.upper()
+            elif x.isupper():
+                return x.lower()
+            else:
+                return x
+
+        return ''.join([fn(r) for r in list(input)])
+
+    def train(self, times):
+        for i in range(1, times):
+            out = self.data_sel[random.randint(0, len(self.data_sel) - 1)]
+            raw_input(self.trans(out))
+            raw_input(out)
 
 
 if __name__ == "__main__":
-    load_raw("HWL.txt")
-    select('OP')
-    train(50)
+    try:
+        os.system('cls')
+        client = Client()
+        client.cmdloop()
+    except:
+        exit()
