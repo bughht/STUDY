dab1f327d28bd92e63b44ba1e2a6355dcb241cb0
bughht <harryhht0@outlook.com>
2018-07-20 09:26:46 +0800

    yeah

--- killwords.py
+++ killwords.py
@@ -10,16 +10,26 @@ def cutfile(input,filename):
     with open(filename,'r') as r:
         lines=r.readlines()
     with open(filename,'w') as w:
-        for l == lines:
-            if input not in l:
+        for l in lines:
+            if input == l:
                 w.write(l)
 
-if __name__=="__main__":
+def cuttrain(train_time):
+    for time in range(1,train_time):
+        out = data_sel[random.randint(0, len(data_sel) - 1)]
+        raw_input(trans(out))
+        result=raw_input(out+"\n"+"[y/n]:")
+        if result=="y":
+            cutfile(out,cut_file)
+        else:
+            continue
+
+if __name__ == "__main__":
     cut_file=raw_input("Input Cut file:")
-    load_file(cut_file)
+    load_raw(cut_file)
     letter_sel=raw_input("Head:")
     select(letter_sel)
     print "there are "+str(countsel())+" letters to kill"
     train_time=0
     train_time=int(raw_input("Times?:"))
-    train(train_time)
+    cuttrain(train_time)
